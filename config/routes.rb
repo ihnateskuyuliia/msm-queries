0965b10fd00dc5e65f14249f6b84262b9d9ba668
ihnateskuyuliia <yuliiaihnatesku@uchicago.edu>
2024-11-01 14:21:09 +0000

    save

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get("/", { :controller => "misc", :action => "homepage" })
 
+  get("/directors", { :controller => "directors", :action=> "index"})
+
   get("/actors", { :controller => "film", :action => "actors"})
 end
